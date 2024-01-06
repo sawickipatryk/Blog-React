c1e678b5b28132d70c1898d780eec16c70822913
Patryk Sawicki <sawicki.patryk129@gmail.com>
2024-01-06 22:44:57 +0100

    added Routes and Route to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react'
 
 import MainPage from './pages/MainPage/MainPage'
+import SignIn from './pages/SignIn/SignIn'
+import SignUp from './pages/SignUp/SignUp'
+
+import { Routes, Route } from 'react-router-dom'
 
 function App () {
   return (
     <>
-      <MainPage/>
+      <Routes>
+        <Route
+          path={'*'}
+          element={
+            <MainPage/>
+        }
+        />
+        <Route
+          path={'/login'}
+          element={
+            <SignIn/>
+        }
+        />
+        <Route
+          path={'/register'}
+          element={
+            <SignUp/>
+        }
+        />
+      </Routes>
+
     </>
   )
 }
