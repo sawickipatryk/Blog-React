38c5be81747d62c84dcad6c6f47b881b0f5dd4fe
Patryk Sawicki <sawicki.patryk129@gmail.com>
2024-01-07 21:34:38 +0100

    removed old arrays and added new from fetch

--- src/components/OurPosts/OurPosts.js
+++ src/components/OurPosts/OurPosts.js
@@ -2,92 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { Box, Grid, Typography, Container } from '@mui/material'
-import { Temporal } from '@js-temporal/polyfill'
 import theme from '../../theme'
-import post1 from './post1.jpg'
-
-const postsArray = [
-  {
-    id: 1,
-    tilte: 'Creative photography ideas from smart devices',
-    img: post1,
-    author: 'Patryk Sawicki',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum libero ipsum repellat aliquid enim molestias inventore tenetur at magni asperiores?',
-    NewDate: Temporal.Now.plainDateISO().toString()
-  },
-  {
-    id: 2,
-    tilte: 'Have good food and good taste then enjoy reading',
-    img: post1,
-    author: 'Patryk Sawicki',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum libero ipsum repellat aliquid enim molestias inventore tenetur at magni asperiores?',
-    NewDate: Temporal.Now.plainDateISO().toString()
-  },
-  {
-    id: 3,
-    tilte: 'Have good food and good taste then enjoy reading',
-    img: post1,
-    author: 'Patryk Sawicki',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum libero ipsum repellat aliquid enim molestias inventore tenetur at magni asperiores?',
-    NewDate: Temporal.Now.plainDateISO().toString()
-  }
-]
-const secondPostsArray = [
-  {
-    id: 1,
-    tilte: 'Creative photography ideas from smart devices',
-    img: post1,
-    author: 'Patryk Sawicki',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum libero ipsum repellat aliquid enim molestias inventore tenetur at magni asperiores?',
-    NewDate: Temporal.Now.plainDateISO().toString()
-  },
-  {
-    id: 2,
-    tilte: 'Have good food and good taste then enjoy reading',
-    img: post1,
-    author: 'Patryk Sawicki',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum libero ipsum repellat aliquid enim molestias inventore tenetur at magni asperiores?',
-    NewDate: Temporal.Now.plainDateISO().toString()
-  },
-  {
-    id: 3,
-    tilte: 'Have good food and good taste then enjoy reading',
-    img: post1,
-    author: 'Patryk Sawicki',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum libero ipsum repellat aliquid enim molestias inventore tenetur at magni asperiores?',
-    NewDate: Temporal.Now.plainDateISO().toString()
-  },
-  {
-    id: 4,
-    tilte: 'Have good food and good taste then enjoy reading',
-    img: post1,
-    author: 'Patryk Sawicki',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum libero ipsum repellat aliquid enim molestias inventore tenetur at magni asperiores?',
-    NewDate: Temporal.Now.plainDateISO().toString()
-  },
-  {
-    id: 5,
-    tilte: 'Have good food and good taste then enjoy reading',
-    img: post1,
-    author: 'Patryk Sawicki',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum libero ipsum repellat aliquid enim molestias inventore tenetur at magni asperiores?',
-    NewDate: Temporal.Now.plainDateISO().toString()
-  },
-  {
-    id: 6,
-    tilte: 'Have good food and good taste then enjoy reading',
-    img: post1,
-    author: 'Patryk Sawicki',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum libero ipsum repellat aliquid enim molestias inventore tenetur at magni asperiores?',
-    NewDate: Temporal.Now.plainDateISO().toString()
-  }
-]
 
 export const OurPosts = (props) => {
   const {
     sx,
+    posts,
     ...otherProps
   } = props
+
+  console.log(posts)
 
   return (
     <Box
@@ -112,60 +36,61 @@ export const OurPosts = (props) => {
           }}
         >
           {
-          postsArray.map((post) => {
-            return (
-              <Grid
-                key={post.id}
-                item
-              >
-                <Box
-                  sx={{
-                    position: 'relative',
-                    backgroundImage: `url(${post.img})`,
-                    height: '280px',
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                    display: 'flex',
-                    alignItems: 'flex-end',
-                    padding: '10px'
-
-                  }}
+          posts && posts.firstArray
+            .map((post) => {
+              return (
+                <Grid
+                  key={post.id}
+                  item
                 >
                   <Box
-                    color={'white'}
+                    sx={{
+                      position: 'relative',
+                      backgroundImage: `url(${post.img})`,
+                      height: '280px',
+                      backgroundPosition: 'center',
+                      backgroundSize: 'cover',
+                      backgroundRepeat: 'no-repeat',
+                      display: 'flex',
+                      alignItems: 'flex-end',
+                      padding: '10px'
+
+                    }}
                   >
-                    <Typography
-                      variant={'h6'}
-                      sx={{
-                        maxWidth: '350px'
-                      }}
-                    >
-                      {post.tilte}
-                    </Typography>
                     <Box
-                      sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'space-between'
-                      }}
+                      color={'white'}
                     >
                       <Typography
-                        variant={'caption'}
+                        variant={'h6'}
+                        sx={{
+                          maxWidth: '350px'
+                        }}
                       >
-                        {post.author}
+                        {post.tilte}
                       </Typography>
-                      <Typography
-                        variant={'caption'}
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'space-between'
+                        }}
                       >
-                        {post.NewDate}
-                      </Typography>
+                        <Typography
+                          variant={'caption'}
+                        >
+                          {post.author}
+                        </Typography>
+                        <Typography
+                          variant={'caption'}
+                        >
+                          {post.NewDate}
+                        </Typography>
+                      </Box>
                     </Box>
                   </Box>
-                </Box>
-              </Grid>
-            )
-          })
+                </Grid>
+              )
+            })
         }
         </Grid>
         <Typography
@@ -181,69 +106,69 @@ export const OurPosts = (props) => {
           container
         >
           {
-          secondPostsArray.map((post) => {
-            return (
-              <Grid
-                key={post.id}
-                item
-                xs={12}
-                sm={6}
-                md={4}
-              >
-                <Box
-                  color={'black'}
-                  sx={{
-                    display: 'flex'
-                  }}
-                >
-                  <Box
-                    sx={{
-                      width: '150px'
-                    }}
-                  >
-                    <img
-                      src={post.img}
-                      alt={'something'}
-                      style={{
-                        height: '100%',
-                        objectFit: 'cover'
-                      }}
-                    />
-                  </Box>
-                  <Box
-                    sx={{
-                      padding: '0px 10px 30px'
-                    }}
-                  >
-                    <Typography
-                      variant={'body2'}
-                      fontWeight={theme.typography.fontWeightBold}
-                    >
-                      {post.tilte}
-                    </Typography>
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'space-between'
-                      }}
-                    >
-                      <Typography
-                        variant={'caption'}
-                      >
-                        {post.author}
-                      </Typography>
-                      <Typography
-                        variant={'caption'}
-                      >
-                        {post.NewDate}
-                      </Typography>
-                    </Box>
-                  </Box>
-                </Box>
-              </Grid>
-            )
-          })
+           posts && posts.secondArray.map((post) => {
+             return (
+               <Grid
+                 key={post.id}
+                 item
+                 xs={12}
+                 sm={6}
+                 md={4}
+               >
+                 <Box
+                   color={'black'}
+                   sx={{
+                     display: 'flex'
+                   }}
+                 >
+                   <Box
+                     sx={{
+                       width: '150px'
+                     }}
+                   >
+                     <img
+                       src={post.img}
+                       alt={'something'}
+                       style={{
+                         height: '100%',
+                         objectFit: 'cover'
+                       }}
+                     />
+                   </Box>
+                   <Box
+                     sx={{
+                       padding: '0px 10px 30px'
+                     }}
+                   >
+                     <Typography
+                       variant={'body2'}
+                       fontWeight={theme.typography.fontWeightBold}
+                     >
+                       {post.tilte}
+                     </Typography>
+                     <Box
+                       sx={{
+                         display: 'flex',
+                         alignItems: 'center',
+                         justifyContent: 'space-between'
+                       }}
+                     >
+                       <Typography
+                         variant={'caption'}
+                       >
+                         {post.author}
+                       </Typography>
+                       <Typography
+                         variant={'caption'}
+                       >
+                         {post.NewDate}
+                       </Typography>
+                     </Box>
+                   </Box>
+                 </Box>
+               </Grid>
+             )
+           })
         }
         </Grid>
       </Container>
@@ -252,7 +177,10 @@ export const OurPosts = (props) => {
 }
 
 OurPosts.propTypes = {
-  sx: PropTypes.object
+  sx: PropTypes.object,
+  slicedArray: PropTypes.object,
+  posts: PropTypes.object
+
 }
 
 export default OurPosts
