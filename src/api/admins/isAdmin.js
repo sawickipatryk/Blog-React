c87193ae5c2309fe12b0eaba953a2dba31cd5b84
Patryk Sawicki <sawicki.patryk129@gmail.com>
2024-01-09 20:12:47 +0100

    console.log deleted

--- src/api/admins/isAdmin.js
+++ src/api/admins/isAdmin.js
@@ -1,7 +1,6 @@
 export const isAdmin = async (userId) => {
   const adminValue = await fetch(`${process.env.REACT_APP_FIREBASE_URL}/blog/admins/${userId}.json`)
   const data = await adminValue.json()
-  console.log(data)
   return data === true
 }
 export default isAdmin
