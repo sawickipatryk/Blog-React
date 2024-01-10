7cb4e9cf96919bb2dc8e233c973deec57f50afa9
Patryk Sawicki <sawicki.patryk129@gmail.com>
2024-01-10 10:58:05 +0100

    added table and items to  PageAdminBlogs

--- src/pages/PageAdminBlogs/PageAdminBlogs.js
+++ src/pages/PageAdminBlogs/PageAdminBlogs.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { Box } from '@mui/material'
+import {
+  Box,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  Paper,
+  TableBody,
+  IconButton,
+  Container
+} from '@mui/material'
+import { useSelector } from 'react-redux'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import theme from '../../theme'
 
 export const PageAdminBlogs = (props) => {
   const {
@@ -9,14 +24,78 @@ export const PageAdminBlogs = (props) => {
     ...otherProps
   } = props
 
+  const { data } = useSelector((state) => state.posts)
+
+  console.log(data)
+
   return (
     <Box
       sx={{
         ...sx
+
       }}
       {...otherProps}
     >
-      PageAdminBlogs
+      <Container
+        maxWidth={'lg'}
+        sx={{
+          paddingTop: '40px'
+        }}
+      >
+        <TableContainer component={Paper}>
+          <Table
+            sx={{
+              minWidth: 650
+            }}
+            aria-label={'simple table'}
+          >
+            <TableHead>
+              <TableRow
+                sx={{
+                  fontWeight: theme.typography.fontWeightBold
+                }}
+              >
+                <TableCell
+                  sx={{ color: 'black' }}
+                >Title
+                </TableCell>
+                <TableCell
+                  sx={{ color: 'black' }}
+                  align={'right'}
+                >Action
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data && data.map((item) => (
+                <TableRow
+                  key={item.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell
+                    component={'th'}
+                    scope={'row'}
+                    sx={{
+                      color: 'black',
+                      fontWeight: theme.typography.fontWeightBold
+                    }}
+                  >
+                    {item.tilte}
+                  </TableCell>
+                  <TableCell align={'right'}>
+                    <IconButton>
+                      <EditIcon/>
+                    </IconButton>
+                    <IconButton>
+                      <DeleteIcon/>
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Container>
     </Box>
   )
 }
